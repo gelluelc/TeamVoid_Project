d00660353cf1e42650fd6e9030c877d01082f8b2
Gelluel Cabingao <acerzebra3@gmail.com>
2021-07-18 16:38:56 +0800

    test

--- app/src/main/java/com/example/teamvoidproject/MainActivity.java
+++ app/src/main/java/com/example/teamvoidproject/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.teamvoidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialization();
+        listeners();
+    }
+
+    public void initialization(){
+        btn_register = (Button) findViewById(R.id.id_RegisterButton);
+    }
+
+    public void listeners(){
+        btn_register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.id_RegisterButton:
+                startActivity(new Intent(this,register.class));
+                break;
+        }
     }
 }
